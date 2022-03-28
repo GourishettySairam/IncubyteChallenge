8a95126a491bea87da313e14c89e6488c2ece365
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-03-28 13:08:21 +0530

    created frontend folder using create-react-app

--- backend/wordcrud/views.py
+++ backend/wordcrud/views.py
@@ -1,3 +1,9 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
+from django.http.response import HttpResponse
+
 # Create your views here.
+
+def greeting(request):
+    return HttpResponse('Hi Sairam')
