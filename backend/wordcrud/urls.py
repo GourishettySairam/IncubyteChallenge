4ad068f71d8245a8b00b0b290762af34813ec7f2
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2022-03-28 15:05:17 +0530

    completed functionality for updateWord and wordList functions

--- backend/wordcrud/urls.py
+++ backend/wordcrud/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url, include
-from .views import wordList
+from numpy import delete
+from .views import wordList, updateWord, deleteWord, createWord
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    url(r'^list/$', wordList, name='all-word-list'),
+    url(r'^list/$', csrf_exempt(wordList), name='all-word-list'),
+    url(r'^updateWord/(?P<pk>[\d]+)/$', csrf_exempt(updateWord), name='update-word'),
+    url(r'^createWord/$', csrf_exempt(createWord), name='create-word'),
+    url(r'^deleteWord/(?P<pk>[\d]+)/$', csrf_exempt(deleteWord), name='delete-word')
 ]
--- backend/wordcrud/views.py
+++ backend/wordcrud/views.py
@@ -2,6 +2,10 @@ from http.client import HTTPResponse
 from django.shortcuts import render
 
 from django.http.response import HttpResponse
+from .models import Words
+from django.core import serializers
+
+import json
 
 # Create your views here.
 
@@ -9,4 +13,29 @@ def greeting(request):
     return HttpResponse('Hi Sairam')
 
 def wordList(request):
-    return HttpResponse('returning a list of words')
+    if request.method == 'GET':
+        print('get request received')
+    tmpJson = serializers.serialize("json", Words.objects.all())
+    tmpObj = json.loads(tmpJson)
+    return HttpResponse(json.dumps(tmpObj))
+
+def updateWord(request, pk):
+    if(request.method == "PUT"):
+        print(Words.objects.get(pk=pk))
+        temp = Words.objects.get(pk=pk)
+
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        content = body['word']
+        print(content)
+        temp.word = content
+        temp.save()
+        tmpJson = serializers.serialize("json", Words.objects.all())
+        tmpObj = json.loads(tmpJson)
+        return HttpResponse(json.dumps(tmpObj))
+
+def createWord(request):
+    return HttpResponse('word created')
+
+def deleteWord(request, pk):
+    return HttpResponse('word deleted')
